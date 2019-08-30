cfad9f073e4a617872fbb72cb83d6502c5b81de5
CroGamer12 <48877715+CroGamer12@users.noreply.github.com>
2019-08-30 14:25:10 +0200

    Add files via upload

--- js/mixer_channel_updates.js
+++ js/mixer_channel_updates.js
@@ -1,35 +1,39 @@
-import { Carina } from "./carina.min.importable";
-import alerts from "./index"
+import { Carina } from './carina.min.importable';
+import alerts from './index';
 
-const selectChannel = (user) => {
+const selectChannel = user => {
     fetch(`https://mixer.com/api/v1/channels/${user}?fields=id`)
-        .then (res => {
+        .then(res => {
             return res.json();
         })
-        .then (data => {
+        .then(data => {
             test(data.id);
-        })
-}
+        });
+};
 
-const test = (channelId) => {
+const test = channelId => {
     const ca = new Carina({
-        isBot: true,
+        isBot: true
     }).open();
 
     ca.subscribe(`channel:${channelId}:followed`, data => {
-        alerts("_true", data.user.username, "_false", "_false", "_false", "_false", "_false", "_false", "_false");
+        console.log(data);
+        alerts('_true', data.user.username, '_false', '_false', '_false', '_false', '_false', '_false', '_false');
     });
     ca.subscribe(`channel:${channelId}:hosted`, data => {
-        alerts("_false", "_false", "_true", data.hoster.token, "_false", "_false", "_false", "_false", "_false");
+        console.log(data);
+        alerts('_false', '_false', '_true', data.hoster.token, '_false', '_false', '_false', '_false', '_false');
     });
     ca.subscribe(`channel:${channelId}:subscribed`, data => {
-        alerts("_false", "_false", "_false", "_false", "_true", data.user.username, "_false", "_false", "_false");
+        console.log(data);
+        alerts('_false', '_false', '_false', '_false', '_true', data.user.username, '_false', '_false', '_false');
     });
     ca.subscribe(`channel:${channelId}:resubscribed`, data => {
-        alerts("_false", "_false", "_false", "_false", "_false", "_false", "_true", data.user.username, "_false");
+        console.log(data);
+        alerts('_false', '_false', '_false', '_false', '_false', '_false', '_true', data.user.username, '_false');
     });
-    ca.subscribe(`channel:${channelId}:patronageUpdate`, data => {
-        alerts("_false", "_false", "_false", "_false", "_false", "_false", "_false", "_false", "_true");
+    ca.subscribe(`channel:${channelId}:patronageUpdate`, () => {
+        alerts('_false', '_false', '_false', '_false', '_false', '_false', '_false', '_false', '_true');
     });
-}
+};
 export default selectChannel;
